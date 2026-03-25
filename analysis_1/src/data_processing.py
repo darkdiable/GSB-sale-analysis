ffa393c17072f4b330b34f869c4ce7d5cb668e8a
bilei <darkdiable@163.com>
2026-03-25 18:09:20 +0800

    bugfix

--- analysis_1/src/data_processing.py
+++ analysis_1/src/data_processing.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from config import DATA_PATH
 
 def load_data():
     df_sales = pd.read_csv('data/sales_data.csv')
@@ -10,7 +9,7 @@ def load_data():
 def clean_data(df_sales, df_store):
     df_sales['date'] = pd.to_datetime(df_sales['date'], format='%Y-%m-%d') 
     
-    df_merged = pd.merge(df_sales, df_store) 
+    df_merged = pd.merge(df_sales, df_store, on='store_id') 
     
     threshold = df_merged['sales'].quantile(0.99)
     df_merged = df_merged[df_merged['sales'] < threshold]
